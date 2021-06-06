d465b2492fbe037321cdec742684250f8dadef4a
Changemin <bkm.change.min@gmail.com>
2021-06-06 20:10:55 +0900

    Initial Commit

---
+++ invisible-stopwatch/invisible_stopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  invisible_stopwatchApp.swift
+//  invisible-stopwatch
+//
+//  Created by 변경민 on 2021/06/06.
+//
+
+import SwiftUI
+
+@main
+struct invisible_stopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
